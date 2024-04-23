3b0ee7f9a6406168c9c216e181117f7849c93700
Steven Reddy <steveng.reddy@gmail.com>
2024-04-23 16:17:20 +0100

    Making a start on the Activity page, added the headers and two cards. Made device friendly

---
+++ src/app/Activity/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Activity() {
+    return (
+        <div className="flex-1 w-full">
+            <div className="flex-col items-center text-center mb-3">
+                <h1 className="text-4xl font-bold my-2">Activity</h1>
+                <h2 className="text-2xl">Our Project View On Institutional Care</h2>
+            </div>
+            <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow sm:flex-row sm:max-w-full dark:border-gray-700 dark:bg-gray-800 mb-4">
+                <div className="flex flex-col p-4 leading-normal">
+                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Observation Home For JCL Boys</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">We have started Observation home in the year 2012, January.The all J.C.L Boys of observation home under J.J.act of I.C.P.S are involved with various kind of antisocial activities . The main object of the Govt. is to correct the life of criminal boy by care protection and love white view to bring them in the main stream of life so that the J.C.L Boys may stand at their won fit and prosper in future life. We have already admitted two J.C.L boys in the school. Beside this we have also engaged arts and craft teacher for unschooled J.C.L boys. There is an all arrangement of playing football, volley ball, cricket.14 staff has been engaged to look after the J.C.L Boys. Food clothes are regular supply to the J.C.L Boys and medical treatment are properly done by regd. medical practitioner. Thanks to Govt. of W.B. & Govt. of India for financial assistance.</p>
+                </div>
+                <img className="object-cover w-full rounded-t-lg h-auto sm:h-auto sm:w-52 sm:rounded-none sm:rounded-s-lg" src="http://www.chiranabin.org.in/style/images/pic1.png" alt="image of people" />
+            </div>
+            <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow sm:flex-row sm:max-w-full dark:border-gray-700 dark:bg-gray-800 mb-4">
+                <img className="object-cover w-full rounded-t-lg h-auto sm:h-auto sm:w-52 sm:rounded-none sm:rounded-s-lg" src="http://www.chiranabin.org.in/style/images/pic2.png" alt="image of people playing cricket" />
+                <div className="flex flex-col p-4 leading-normal order-first sm:order-last">
+                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">CHILDREN AND AFTER CARE HOME FOR WOMEN & GIRLS OF ICPS UNDER j.j. act</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">We are running the children home from the year 1998.We at first started the children home (formerly shelter home) with limited MR girls now its strength has reached to the 167. Forty two staffs have been engaged to look after MR girls. There are two categories inmates i.e. mild and moderate .The inmates are homeless parent less, and have no identity. The inmates come from CWC. When the inmates come to the children home they were ill faded and ill clad, and feeble. They cant eat individually and wear garments .Now they are vivacious for proper treatment care protection food lodging etc. It is our great pleasure that we have able retained about 42 inmates to the mother lap. We have made and arrangement of education, recreation, V.T so that the inmates may get relief from monotonous life and enjoy recreation by way of song, music under the guidance of musician and also arrangement of tailoring sewing so that the inmates may be self supported. Special educator teaches the mild category inmates with great care and attentively. It is found the inmates are developing gradually improve in studies, dancing, song and recitation. Inmates participate in the different programmers of our organization. Thanks to Govt. of W.B. & Govt. of India for financial assistance.</p>
+                </div>
+            </div>
+            <div className="flex-col items-center text-center my-3 border-b">
+                <h2 className="text-4xl my-2 font-bold">Our Achievements</h2>
+            </div>        
+        </div>
+    );
+}
