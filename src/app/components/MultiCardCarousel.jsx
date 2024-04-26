c2af3ec024e641ac61edc80df86e1736fc4a089a
Steven Reddy <steveng.reddy@gmail.com>
2024-04-26 16:20:12 +0100

    carsousel works as planned, few alteration to mobile styling and some padding issues

--- src/app/components/MultiCardCarousel.jsx
+++ src/app/components/MultiCardCarousel.jsx
@@ -17,18 +17,18 @@ export default function MultiCardCarousel({ cards }) {
     return (
         <div className="overflow-hidden relative w-fill">
             <div
-                className="flex transition ease-out duration-400 "
+                className="flex transition ease-out duration-400 items-center"
                 style={{ transform: `translateX(-${current * 100}%)` }}
             >
                 {cards.map((card, index) => (
-                    <div key={index} className="min-w-[100%] px-24 box-border text-center">
+                    <div key={index} className="min-w-[100%] px-8 md:px-24 box-border text-center">
                         <h2 className="text-3xl mb-3">{card.title}</h2>
                         <p className="mb-12">{card.content}</p>
                     </div>
                 ))}
             </div>
 
-            <div className="absolute top-0 h-full w-full flex justify-between items-center px-10 text-3xl">
+            <div className="absolute top-0 h-full w-full flex justify-between items-center md:px-10 text-3xl">
                 <button onClick={previousSlide}>
                     <FaArrowAltCircleLeft />
                 </button>
@@ -37,7 +37,7 @@ export default function MultiCardCarousel({ cards }) {
                 </button>
             </div>
 
-            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+            <div className="absolute bottom-0 p-4 flex justify-center gap-3 w-full">
                 {cards.map((_, index) => (
                     <div
                         key={index}
