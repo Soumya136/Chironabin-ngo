1c528e8ed90052e8305c8eff1bcb7b6f55d818e7
Steven Reddy <steveng.reddy@gmail.com>
2024-04-30 11:15:35 +0100

    adding a photo carousel, styling need considering and photos need process and saving to public

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import PhotoCarousel from '../components/PhotoCarousel.jsx'
 
 const Home = () => {
+  const slides = [
+    'http://www.chiranabin.org.in/style/images/art/s2.jpg',
+    'http://www.chiranabin.org.in/style/images/art/s1.jpg',
+    'http://www.chiranabin.org.in/style/images/art/s3.jpg',
+    'http://www.chiranabin.org.in/style/images/art/s4.jpg',
+    'http://www.chiranabin.org.in/style/images/art/s5.jpg',
+    'http://www.chiranabin.org.in/style/images/art/s6.jpg'
+  ]
   return (
-    <div>
-        
-      
-    </div>
+    <>
+      <PhotoCarousel slides={slides}/>
+
+    </>
   )
 }
 
